51201846243e3df96e530cc485a5a0136d78651b
peng <1343296290@qq.com>
2018-05-31 14:48:55 +0800

    first commit

---
+++ src/main/java/com/generator/codegen/annotation/FreemarkerComponent.java
@@ -0,0 +1,20 @@
+package com.generator.codegen.annotation;
+
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author Pengxiaopeng
+ * @DESCRIPTION todo
+ * @create 2018-05-31 11:39
+ **/
+@Target(value = ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Component
+public @interface FreemarkerComponent {
+    String value() default "";
+}
