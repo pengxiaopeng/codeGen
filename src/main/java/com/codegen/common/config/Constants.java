cd577accad9ca0f5a20847a2ebf5ab35b95480cf
peng <1343296290@qq.com>
2018-06-05 16:07:19 +0800

    first commit

--- src/main/java/com/codegen/common/config/Constants.java
+++ src/main/java/com/codegen/common/config/Constants.java
@@ -4,5 +4,8 @@ public final class Constants {
 
     public final static String SUCCESS_ICON = "1";
     public final static String FAIL_ICON = "5";
+    public final static String ICON_NAME = "icon";
+    public final static String MESSAGE_NAME = "message";
+
 
 }
