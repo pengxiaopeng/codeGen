c7b5f3d61ddd3e3659b2afc1223948415756539e
pengxiaopeng <1343296290@qq.com>
2018-06-20 20:12:03 +0800

    first commit

--- src/main/java/com/codegen/common/web/CommonContrller.java
+++ src/main/java/com/codegen/common/web/CommonContrller.java
@@ -1,17 +1,8 @@
 package com.codegen.common.web;
 
-import com.codegen.modules.model.Authority;
-import com.codegen.modules.model.Menu;
 import com.codegen.modules.service.*;
-import com.codegen.modules.model.Authority;
-import com.codegen.modules.model.Menu;
-import com.codegen.modules.service.*;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.ui.Model;
-
-import java.util.List;
 
 /**
  * @author Pengxiaopeng
@@ -20,8 +11,6 @@ import java.util.List;
  **/
 @Component
 public class CommonContrller {
-    @Autowired
-    public MenuService menuService;
     @Autowired
     public AuthorityService authorityService;
     @Autowired
@@ -35,61 +24,4 @@ public class CommonContrller {
     @Autowired
     public OrgService orgService;
 
-    public String getPageStr(Model model, String pageAddress) {
-        model.addAttribute("authorityList", getAuthorityList());
-        return pageAddress;
-    }
-
-    /**
-     * @param menuId   指定 id 选中
-     * @param menuType 1 侧边栏菜单  2 顶部菜单
-     * @return
-     */
-    private List<Menu> getMenuList(String menuId, String menuType) {
-        if (StringUtils.isNotBlank(menuId) && StringUtils.isNotBlank(menuType)) {
-            Menu menu = new Menu();
-            menu.setMenuType(menuType);
-            menu.setIsRoot("1");
-            List<Menu> list = menuService.findList(menu);
-            if (list != null && !list.isEmpty()) {
-                for (Menu menu1 : list) {
-                    if (StringUtils.equals(menu1.getId(), menuId)) {
-                        menu1.setIsChoose("1");
-                    }
-                    Menu menu2 = new Menu();
-                    menu2.setParentId(menu1.getId());
-                    List<Menu> menus = menuService.findList(menu2);
-                    for (Menu menu3 : menus) {
-                        if (StringUtils.equals(menu3.getId(), menuId)) {
-                            menu3.setIsChoose("1");
-                        }
-                    }
-                    menu1.setChildMenuList(menus);
-                }
-            }
-            return list;
-        }
-        return null;
-    }
-    /**
-     * 获取菜单列表
-     * @return
-     */
-    public List<Authority> getAuthorityList() {
-        List<Authority> parentList = getParentAuthorityList();
-        if (parentList != null && !parentList.isEmpty()) {
-            for (Authority authority1 : parentList) {
-                authority1.setChildAuthorityList(authorityService.findList(new Authority(authority1.getId())));
-            }
-        }
-        return parentList;
-    }
-
-    /**
-     * 获取父级菜单
-     * @return
-     */
-    public List<Authority> getParentAuthorityList() {
-        return authorityService.findList(new Authority(0L));
-    }
 }
--- src/main/java/com/codegen/modules/service/MenuService.java
+++
@@ -1,7 +0,0 @@
-package com.codegen.modules.service;
-
-import com.codegen.common.base.IBaseService;
-import com.codegen.modules.model.Menu;
-
-public interface MenuService extends IBaseService<Menu, String> {
-}
