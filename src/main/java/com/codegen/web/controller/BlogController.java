6a8ba9c57129bfcff02eda57066654758cb3d465
1343296290@qq.com <1343296290@qq.com>
2018-07-02 09:47:14 +0800

    first commit

---
+++ src/main/java/com/codegen/web/controller/BlogController.java
@@ -0,0 +1,17 @@
+package com.codegen.web.controller;
+
+import com.codegen.common.web.CommonContrller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Controller - bid_blog
+ * @author	xuyb
+ * @version 2.0
+ * @date 	2018-07-02
+ */
+@Controller("BlogController")
+@RequestMapping("/admin/blog")
+public class BlogController  extends CommonContrller {
+
+}
