55e90e10a452aa854b19cd15f31f414a7b9de439
1343296290@qq.com <1343296290@qq.com>
2018-07-02 16:46:18 +0800

    first commit

--- src/main/java/com/codegen/modules/model/Blog.java
+++ src/main/java/com/codegen/modules/model/Blog.java
@@ -8,13 +8,41 @@ import com.codegen.common.model.BaseModel;
  * @create 2018-07-02 9:34
  **/
 public class Blog extends BaseModel {
+    private Long id;
     private String content ;
     private String label ;
     private String type ;
+    private String author;
+    private String title ;
     private java.util.Date modifydate ;
     private java.util.Date createdate ;
     private Long modifyuserid ;
     private Long createuserid ;
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public void setContent (String content){
         this.content = content ;
     }
